b563e623a22c68d85ff468dc4e2699578356192f
GOWTHAM1607 <94114067+GOWTHAM1607@users.noreply.github.com>
2022-01-20 17:58:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+ 
+       function change() {
+               document.getElementById("body").style.backgroundColor = "red";
+           name="Rishy";
+               document.getElementById("heading").innerHTML=name
+           age=11;
+           document.getElementById(
+           "age").innerHTML=age
+            }
+            
